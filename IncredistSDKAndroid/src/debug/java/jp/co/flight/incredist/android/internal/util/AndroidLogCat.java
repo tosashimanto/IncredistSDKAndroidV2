46965efa6c3122330c544dd4d17b8da0a1ca9c76
HIDAKA Takahiro <t-hidaka@flight.co.jp>
2018-01-04 14:51:38 +0900

    release ビルド時には LogCat へ出力しないように実装

---
+++ IncredistSDKAndroid/src/debug/java/jp/co/flight/incredist/android/internal/util/AndroidLogCat.java
@@ -0,0 +1,48 @@
+package jp.co.flight.incredist.android.internal.util;
+
+import android.util.Log;
+
+/**
+ * LogCat へそのまま出力するためのクラス
+ */
+public class AndroidLogCat implements LogInterface {
+    public int d(String tag, String msg, Throwable tr) {
+        return Log.d(tag, msg, tr);
+    }
+
+    public int d(String tag, String msg) {
+        return Log.d(tag, msg);
+    }
+
+    public int e(String tag, String msg, Throwable tr) {
+        return Log.e(tag, msg, tr);
+    }
+
+    public int e(String tag, String msg) {
+        return Log.e(tag, msg);
+    }
+
+    public int i(String tag, String msg, Throwable tr) {
+        return Log.i(tag, msg, tr);
+    }
+
+    public int i(String tag, String msg) {
+        return Log.i(tag, msg);
+    }
+
+    public int v(String tag, String msg, Throwable tr) {
+        return Log.v(tag, msg, tr);
+    }
+
+    public int v(String tag, String msg) {
+        return Log.v(tag, msg);
+    }
+
+    public int w(String tag, String msg, Throwable tr) {
+        return Log.w(tag, msg, tr);
+    }
+
+    public int w(String tag, String msg) {
+        return Log.w(tag, msg);
+    }
+}
