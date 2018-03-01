783fb3972ff9c47865c5137b5e55462c8f9dbe18
HIDAKA Takahiro <t-hidaka@flight.co.jp>
2018-03-01 14:10:40 +0900

    FeliCa の応答パケットが 1byte ずれていたのを対応

--- IncredistSDKAndroid/src/main/java/jp/co/flight/incredist/android/internal/controller/result/FelicaCommandResult.java
+++ IncredistSDKAndroid/src/main/java/jp/co/flight/incredist/android/internal/controller/result/FelicaCommandResult.java
@@ -7,17 +7,19 @@ public class FelicaCommandResult extends IncredistResult {
     public final int status1;
     public final int status2;
     public final byte[] resultData;
+    public final byte[] rawData;
 
     /**
      * コンストラクタ
      *
      * @param resultData FeliCaコマンドの応答データ
      */
-    public FelicaCommandResult(int status1, int status2, byte[] resultData) {
+    public FelicaCommandResult(int status1, int status2, byte[] resultData, byte[] rawData) {
         super(IncredistResult.STATUS_SUCCESS);
 
         this.status1 = status1;
         this.status2 = status2;
         this.resultData = resultData;
+        this.rawData = rawData;
     }
 }
