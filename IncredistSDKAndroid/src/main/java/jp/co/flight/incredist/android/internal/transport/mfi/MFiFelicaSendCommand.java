5650783d77a53045812a45c124abbafe9feeea03
HIDAKA Takahiro <t-hidaka@flight.co.jp>
2018-06-27 18:32:09 +0900

    MFiFelicaSendCommand にログを追加

--- IncredistSDKAndroid/src/main/java/jp/co/flight/incredist/android/internal/transport/mfi/MFiFelicaSendCommand.java
+++ IncredistSDKAndroid/src/main/java/jp/co/flight/incredist/android/internal/transport/mfi/MFiFelicaSendCommand.java
@@ -3,17 +3,22 @@ package jp.co.flight.incredist.android.internal.transport.mfi;
 import android.support.annotation.NonNull;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 import jp.co.flight.incredist.android.internal.controller.result.FelicaCommandResult;
 import jp.co.flight.incredist.android.internal.controller.result.IncredistResult;
+import jp.co.flight.incredist.android.internal.util.FLog;
 
 /**
  * MFi版 FeliCa コマンドパケット送信コマンド.
  */
 public class MFiFelicaSendCommand extends MFiCommand {
+    private static final String TAG = "MFiFelicaSendCommand";
     private static final byte[] FSOC_HEADER = {'f', 's', 'o', 'c'};
 
-    private static long mWait;
+    private final int mWaitParam;
+    private final long mCreatedTime;
+    private final long mWait;
 
     /**
      * コマンドパケット生成
@@ -23,7 +28,6 @@ public class MFiFelicaSendCommand extends MFiCommand {
      */
     private static byte[] createPayload(byte wait, byte[] felicaCommand) {
         // CHECKSTYLE:OFF MagicNumber
-        mWait = wait * 5 + 2000;
         byte[] payload = new byte[felicaCommand.length + 7];
 
         System.arraycopy(FSOC_HEADER, 0, payload, 0, FSOC_HEADER.length);
@@ -43,6 +47,9 @@ public class MFiFelicaSendCommand extends MFiCommand {
      */
     public MFiFelicaSendCommand(int wait, byte[] felicaCommand) {
         super(createPayload((byte) wait, felicaCommand));
+        mWaitParam = wait;
+        mWait = wait * 5 + 2000;
+        mCreatedTime = System.currentTimeMillis();
     }
 
     /**
@@ -52,7 +59,7 @@ public class MFiFelicaSendCommand extends MFiCommand {
      */
     @Override
     public long getResponseTimeout() {
-        return mWait;  // SUPPRESS CHECKSTYLE MagicNumber
+        return mWait;
     }
 
     /**
@@ -83,10 +90,14 @@ public class MFiFelicaSendCommand extends MFiCommand {
             int length = bytes[6] & 0xff;
 
             if (length == 0 && bytes.length == 7) {
+                FLog.d(TAG, String.format(Locale.JAPANESE, "wait param:%d wait:%d real:%d length: %d", mWaitParam, mWait, System.currentTimeMillis() - mCreatedTime, bytes.length));
+
                 return new FelicaCommandResult(bytes[4] & 0xff, bytes[5] & 0xff,
                         new byte[]{},
                         Arrays.copyOfRange(bytes, 4, bytes.length));
             } else if (bytes[0] == 'f' && bytes[1] == 's' && bytes[2] == 'o' && bytes[3] == 'c' && bytes.length == length + 7) {
+                FLog.d(TAG, String.format(Locale.JAPANESE, "wait param:%d wait:%d real:%d length: %d", mWaitParam, mWait, System.currentTimeMillis() - mCreatedTime, bytes.length));
+
                 return new FelicaCommandResult(bytes[4] & 0xff, bytes[5] & 0xff,
                         Arrays.copyOfRange(bytes, 8, bytes.length),
                         Arrays.copyOfRange(bytes, 4, bytes.length));
